1c2705314bec214239c5d32c22e9785605301292
Nia <kikyoporter14@yahoo.com>
2014-11-06 12:23:09 -0500

    no message

--- 0_TestBed/BoundingBoxManager.cpp
+++ 0_TestBed/BoundingBoxManager.cpp
@@ -1,11 +1,185 @@
 #include "BoundingBoxManager.h"
 
+BoundingBoxManager* BoundingBoxManager::bInstance = nullptr;
 
-BoundingBoxManager::BoundingBoxManager(void)
+BoundingBoxManager* BoundingBoxManager::GetInstance()
 {
+	if(bInstance == nullptr)
+	{
+		bInstance = new BoundingBoxManager();
+	}
+	return bInstance;
 }
 
-
-BoundingBoxManager::~BoundingBoxManager(void)
+void BoundingBoxManager::ReleaseInstance()
 {
+	if(bInstance != nullptr)
+	{
+		delete bInstance;
+		bInstance = nullptr;
+	}
+}
+
+void BoundingBoxManager::Init(void)
+{
+	bModelMngr = ModelManagerClass::GetInstance();
+	bCollidingNames.clear();
+	numBox = 0;
+}
+
+void BoundingBoxManager::Release(void)
+{
+	RemoveBox("ALL");
+	return;
+}
+
+//The big 3
+BoundingBoxManager::BoundingBoxManager(){Init();}
+BoundingBoxManager::BoundingBoxManager(BoundingBoxManager const& other){ }
+BoundingBoxManager& BoundingBoxManager::operator=(BoundingBoxManager const& other) { return *this; }
+BoundingBoxManager::~BoundingBoxManager(){Release();};
+//Accessors
+int BoundingBoxManager::GetNumberOfBoxes(void){ return numBox; }
+//--- Non Standard Singleton Methods
+
+void BoundingBoxManager::SetVisible(bool a_bVisible, String a_sInstance)
+{
+	if(a_sInstance == "ALL")
+	{
+		int numBox = GetNumberOfBoxes();
+		for(int nBox = 0; nBox < numBox; nBox++)
+		{
+			boundingBox[nBox]->SetVisible(a_bVisible);
+		}
+	}
+	else
+	{
+		int box = bModelMngr->IdentifyInstance(a_sInstance);
+		if(box < 0 || box < numBox)
+			boundingBox[box]->SetVisible(a_bVisible);
+	}
+}
+void BoundingBoxManager::SetColor(vector3 a_v3Color, String a_sInstance)
+{
+	if(a_sInstance == "ALL")
+	{
+		int nBoxs = GetNumberOfBoxes();
+		for(int nBox = 0; nBox < nBoxs; nBox++)
+		{
+			boundingBox[nBox]->SetColor(a_v3Color);
+		}
+	}
+	else
+	{
+		int nBox = bModelMngr->IdentifyInstance(a_sInstance);
+		if(nBox < 0 || nBox < numBox)
+			boundingBox[nBox]->SetColor(a_v3Color);
+	}
+}
+void BoundingBoxManager::SetModelMatrix(matrix4 a_mModelMatrix, String a_sInstance)
+{
+	if(a_sInstance == "ALL")
+	{
+		int nBoxs = GetNumberOfBoxes();
+		for(int nBox = 0; nBox < nBoxs; nBox++)
+		{
+			boundingBox[nBox]->SetModelMatrix(a_mModelMatrix);
+		}
+	}
+	else
+	{
+		int nBox = bModelMngr->IdentifyInstance(a_sInstance);
+		if(nBox < 0 || nBox < numBox)
+			boundingBox[nBox]->SetModelMatrix(a_mModelMatrix);
+	}
+}
+void BoundingBoxManager::Render(String a_sInstance)
+{
+	
+	if(a_sInstance == "ALL")
+	{
+		int nBoxs = GetNumberOfBoxes();
+		for(int nBox = 0; nBox < nBoxs; nBox++)
+		{
+			boundingBox[nBox]->Render(MEDEFAULT);
+		}
+	}
+	else
+	{
+		int nBox = bModelMngr->IdentifyInstance(a_sInstance);
+		if(nBox < 0 || nBox < numBox)
+			boundingBox[nBox]->Render(MEDEFAULT);
+	}
+}
+void BoundingBoxManager::AddBox(String a_sInstanceName)
+{
+	BoundingBoxClass* oBox = new BoundingBoxClass(a_sInstanceName);
+	boundingBox.push_back(oBox);
+	numBox ++;
+}
+void BoundingBoxManager::RemoveBox(String a_sInstanceName)
+{
+	if(a_sInstanceName == "ALL")
+	{
+		//Clear the vector's element first otherwise there is a memory leak
+		for(int nBox = 0; nBox < numBox; nBox++)
+		{
+			BoundingBoxClass* pBS = boundingBox[nBox];
+			delete pBS;
+		}
+		boundingBox.clear();
+		numBox = 0;
+		return;
+	}
+	std::vector<BoundingBoxClass*> vTemp;
+	int nBox = bModelMngr->IdentifyInstance(a_sInstanceName);
+	if(nBox < 0 || nBox < numBox)
+	{
+		for(int nBox = 0; nBox < numBox; nBox++)
+		{
+			if(nBox != nBox)
+			{
+				vTemp.push_back(boundingBox[nBox]);
+			}
+			else
+			{
+				BoundingBoxClass* pBS = boundingBox[nBox];
+				delete pBS;
+			}
+		}
+	}
+	boundingBox = vTemp;
+	numBox++;
+}
+void BoundingBoxManager::Update(void)
+{
+	bCollidingNames.clear();
+	for(int nBox = 0; nBox < numBox; nBox++)
+	{
+		boundingBox[nBox]->SetColor(MEWHITE);
+	}
+	CollisionCheck();
+	CollisionResponse();
+}
+void BoundingBoxManager::CollisionCheck(void)
+{
+	
+}
+bool BoundingBoxManager::CheckForNameInList(String a_sName)
+{
+	int nNames = static_cast<int>(bCollidingNames.size());
+	for(int nName = 0; nName < nNames; nName++)
+	{
+		if(bCollidingNames[nName] == a_sName)
+			return true;
+	}
+	return false;
+}
+void BoundingBoxManager::CollisionResponse(void)
+{
+	for(int nBox = 0; nBox < numBox; nBox++)
+	{
+		if(CheckForNameInList(boundingBox[nBox]->GetInstanceName()))
+			boundingBox[nBox]->SetColor(MERED);
+	}
 }
--- 0_TestBed/BoundingBoxManager.h
+++ 0_TestBed/BoundingBoxManager.h
@@ -1,8 +1,72 @@
 #pragma once
+
+#ifndef __BOUNDINGBOXMANAGER_H_
+#define __BOUNDINGBOXMANAGER_H_
+
+#include "BoundingBoxClass.h"
+
+using namespace MyEngine;
+
 class BoundingBoxManager
 {
+	int numBox;
+	std::vector<BoundingBoxClass*> boundingBox;
+	ModelManagerClass* bModelMngr;
+	std::vector<String> bCollidingNames;
 public:
+	static BoundingBoxManager* GetInstance();
+	void ReleaseInstance(void);
+	int GetNumberOfBoxes(void);
+	void AddBox(String bInstanceName);
+	/*Remove a sphere from the List in the manager
+	Arguments:
+		a_sInstanceName name of the instance to remove from the List */
+	void RemoveBox(String bInstanceName = "ALL");
+	/*Sets the visibility of the specified Instance
+	Arguments:
+		a_bVisible sets whether the shape will be drawn or not
+		a_sInstanceName identify the shape if ALL is provided then it applies to all shapes*/
+	void SetVisible(bool bVisible, String bInstanceName = "ALL");
+	/*Sets the Color of the specified Instance
+	Arguments:
+		a_v3Color sets the color of the shape to be drawn
+		a_sInstanceName identify the shape if ALL is provided then it applies to all shapes*/
+	void SetColor(vector3 bv3Color, String bInstanceName = "ALL");
+	/*Sets the Model matrix to the object and the shape
+	Arguments:
+		a_mModelMatrix matrix4 that contains the space provided
+		a_sInstanceName identify the shape if ALL is provided then it applies to all shapes*/
+	void SetModelMatrix(matrix4 bModelMatrix, String bInstanceName = "ALL");
+	/*Render the specified shape
+	Arguments:
+		a_sInstanceName identify the shape if ALL is provided then it applies to all shapes*/
+	void Render(String bInstanceName = "ALL");
+	/*Initializes the list of names and check collision and collision resolution*/
+	void Update(void);
+
+private:
+	/*Constructor*/
 	BoundingBoxManager(void);
+	/*Copy Constructor*/
+	BoundingBoxManager(BoundingBoxManager const& other);
+	/*Copy Assignment operator*/
+	BoundingBoxManager& operator=(BoundingBoxManager const& other);
+	/*Destructor*/
 	~BoundingBoxManager(void);
+	/*Releases the allocated memory*/
+	void Release(void);
+	/*Initializes the manager*/
+	void Init(void);
+
+	static BoundingBoxManager* bInstance; // Singleton
+	/*Check the collision within all spheres*/
+	void CollisionCheck(void);
+	/*Response to the collision test*/
+	void CollisionResponse(void);
+	/*Checks whether a name is the List of collisions
+	Arguments
+		a_sName checks this specific name*/
+	bool CheckForNameInList(String bName);
 };
 
+#endif
